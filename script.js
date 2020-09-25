18056bf90689d8dacc0efa48ccf1106a6f8f7e7e
joneskj55 <joneskj55@gmail.com>
2020-09-25 08:13:01 -0500

    add button to get meal; add divs; print meal to dom

--- script.js
+++ script.js
@@ -30,11 +30,15 @@ const meals = [
         "name": "Burgers"
     }
 ];
+const showMealName = document.getElementById('meal-name');
+//const showMealImage = document.getElementById('meal-image');
 
 function getMeal() {
     const randomMeal = Math.floor(Math.random() * meals.length);
     console.log(meals[randomMeal].name);
     //next steps: 1. add images 2. print name and image to dom 3. HTML, CSS
+    showMealName.innerText = meals[randomMeal].name;
+    //showMealImage.innerText = meals[randomMeal].image;
 }
 
 getMeal();
