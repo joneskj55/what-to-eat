8cddb37203c85efc6a81d82f54caa150ce503433
joneskj55 <joneskj55@gmail.com>
2020-09-25 19:28:08 -0500

    add photos for testing; testing json for img

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 const breakfastMeals = [
     {
         "name": "Mac & Cheese",
+        "image": "../avocado-toast.jpg"
     },
     {
         "name": "Pizza",
+        "image": "veggies.jpg"
     },
     {
         "name": "Cook Out",
+        "image": "noodles.jpg"
     },
-    {
-        "name": "Order",
-    },
-    {
-        "name": "Sandwich",
-    },
-    {
-        "name": "Turkey",
-    },
-    {
-        "name": "Chili",
-    },
-    {
-        "name": "Tacos",
-    },
-    {
-        "name": "Spaghetti",
-    },
-    {
-        "name": "Burgers"
-    }
+    // {
+    //     "name": "Order",
+    // },
+    // {
+    //     "name": "Sandwich",
+    // },
+    // {
+    //     "name": "Turkey",
+    // },
+    // {
+    //     "name": "Chili",
+    // },
+    // {
+    //     "name": "Tacos",
+    // },
+    // {
+    //     "name": "Spaghetti",
+    // },
+    // {
+    //     "name": "Burgers"
+    // }
 ];
 const lunchMeals = [
     {
@@ -96,28 +99,28 @@ const dinnerMeals = [
 ];
 
 const showBreakfastName = document.getElementById('breakfast-name');
-//const showBreakfastImage = document.getElementById('breakfast-image');
+const showBreakfastImage = document.getElementById('breakfast-image');
 const showLunchName = document.getElementById('lunch-name');
-//const showLunchImage = document.getElementById('lunch-image');
+const showLunchImage = document.getElementById('lunch-image');
 const showDinnerName = document.getElementById('dinner-name');
-//const showDinnerImage = document.getElementById('dinner-image');
+const showDinnerImage = document.getElementById('dinner-image');
 
 function getBreakfast() {
     const randomMeal = Math.floor(Math.random() * breakfastMeals.length);
     showBreakfastName.innerText = breakfastMeals[randomMeal].name;
-    //showBreakfastImage.innerText = breakfastMeals[randomMeal].image;
+    showBreakfastImage.innerText = breakfastMeals[randomMeal].image;
 }
 
 function getLunch() {
     const randomMeal = Math.floor(Math.random() * lunchMeals.length);
     showLunchName.innerText = lunchMeals[randomMeal].name;
-    //showLunchImage.innerText = lunchMeals[randomMeal].image;
+    showLunchImage.innerText = lunchMeals[randomMeal].image;
 }
 
 function getDinner() {
     const randomMeal = Math.floor(Math.random() * dinnerMeals.length);
     showDinnerName.innerText = dinnerMeals[randomMeal].name;
-    //showDinnerImage.innerText = dinnerMeals[randomMeal].image;
+    showDinnerImage.innerText = dinnerMeals[randomMeal].image;
 }
 
 
