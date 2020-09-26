b06d32d6ad8ef812f4dae746c4589a09d6090bea
joneskj55 <joneskj55@gmail.com>
2020-09-26 00:10:31 -0500

    refactor

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const showBreakfastName = document.getElementById('breakfast-name');
-const showBreakfastImage = document.getElementById('breakfast-image');
-const showLunchName = document.getElementById('lunch-name');
-const showLunchImage = document.getElementById('lunch-image');
-const showDinnerName = document.getElementById('dinner-name');
-const showDinnerImage = document.getElementById('dinner-image');
-
 function getBreakfast() {
     let request = new XMLHttpRequest();
     request.open('GET', 'data/breakfast.json', true);
@@ -12,9 +5,9 @@ function getBreakfast() {
         if (request.status >= 200 && request.status < 400) {
             let data = JSON.parse(request.responseText);
             const randomMeal = Math.floor(Math.random() * data.length);
-            showBreakfastName.innerText = data[randomMeal].name;
+            document.getElementById('breakfast-name').innerText = data[randomMeal].name;
             let img = document.createElement('img');
-            img.src = showBreakfastImage.innerText = data[randomMeal].image;
+            img.src = data[randomMeal].image;
             document.getElementById('breakfast-image').appendChild(img);
         }
     };
@@ -28,9 +21,9 @@ function getLunch() {
         if (request.status >= 200 && request.status < 400) {
             let data = JSON.parse(request.responseText);
             const randomMeal = Math.floor(Math.random() * data.length);
-            showLunchName.innerText = data[randomMeal].name;
+            document.getElementById('lunch-name').innerText = data[randomMeal].name;
             let img = document.createElement('img');
-            img.src = showLunchImage.innerText = data[randomMeal].image;
+            img.src = data[randomMeal].image;
             document.getElementById('lunch-image').appendChild(img);
         }
     };
@@ -44,9 +37,9 @@ function getDinner() {
         if (request.status >= 200 && request.status < 400) {
             let data = JSON.parse(request.responseText);
             const randomMeal = Math.floor(Math.random() * data.length);
-            showDinnerName.innerText = data[randomMeal].name;
+            document.getElementById('dinner-name').innerText = data[randomMeal].name;
             let img = document.createElement('img');
-            img.src = showDinnerImage.innerText = data[randomMeal].image;
+            img.src = data[randomMeal].image;
             document.getElementById('dinner-image').appendChild(img);
         }
     };
