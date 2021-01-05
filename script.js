6132a42e4e239563b44e9bffb0f612f63e746882
MeerHamza1421 <hamzababar37@gmail.com>
2021-01-06 02:35:34 +0500

    add search functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const breakfastImg = document.createElement('img');
 const lunchImg = document.createElement('img');
 const dinnerImg = document.createElement('img');
+let searchedImg = document.createElement('img');
+
 let recipe;
 let ingredients;
 let modalTitle;
@@ -145,20 +147,99 @@ function clearDinnerIngredientsList() {
     document.getElementById('dinner-ingredients').innerHTML = "";
 }
 
+// ------------------------ For searching ---------------------------------
+
+let notFound = document.createElement('p');
+
+let searchingData = async e => {
+    clearSearch();
+    let Data = [];
+    e.preventDefault();
+    let SearchedOption = document.querySelector('#searchedOption').value;
+
+    await fetch(`data/${SearchedOption}.json`)
+        .then(response => response.json())
+        .then(data => data.map(dataChunks => {
+            Data.push(dataChunks);
+        }))
+
+    let SearchedData = document.querySelector('#searchedData').value;
+    notFound.innerHTML = "Nothing was found";
+    notFound.style.color = 'red';
+    notFound.style.display = 'none';
+    document.querySelector('form').append(notFound);
+    let counter = 0;
+    for (let i = 0; i < Data.length; i++) {
+        if (SearchedData === Data[i].name.toLowerCase() ||
+            SearchedData === Data[i].name.toUpperCase() ||
+            SearchedData === Data[i].name) {
+            notFound.style.display = 'none';
+            document.querySelector('#search-image').style.display = 'block';
+            searchedResult(Data[i], SearchedData);
+            break;
+        }
+        else {
+            notFound.style.display = 'none';
+            counter++;
+            document.querySelector('#search-image').style.display = 'none';
+            document.querySelector('#search-click-instructions').classList.add('hidden');
+
+        }
+        if (counter === Data.length) {
+            notFound.style.display = 'block';
+        }
+    }
+
+}
+
+let searchedResult = data => {
+    clearSearch();
+    document.querySelector('#search-click-instructions').classList.remove('hidden');
+    document.querySelector('#search-name').textContent = data.name;
+    searchedImg.src = data.image;
+    searchedImg.alt = "something went wrong";
+    document.querySelector('#search-image').append(searchedImg);
+    document.querySelector('#searched-recipe-header').innerHTML = data.name;
+    let Ol = document.querySelector('#searchedItemIngredients');
+    data.ingredients.map(ing => {
+        let Li = document.createElement('li');
+        Li.innerHTML = ing;
+        Ol.append(Li);
+    })
+    document.querySelector('#search-recipe').innerHTML = data.recipe;
+}
+
+let clearSearch = () => {
+    document.querySelector('#search-name').textContent = "";
+    document.querySelector('#searched-recipe-header').innerHTML = "";
+    document.querySelector('#searchedItemIngredients').innerHTML = "";
+    document.querySelector('#search-recipe').innerHTML = "";
+}
+
+let showSearchedItemRecipe = () => {
+    document.getElementById('search-recipe').classList.remove('hidden');
+    document.getElementById('search-click-instructions').className = 'hidden';
+}
+
+
+
 // Get the modal
 const modal = document.getElementById("myModal");
 const lunchModal = document.getElementById("lunchModal");
 const dinnerModal = document.getElementById("dinnerModal");
+const searchModal = document.querySelector('#searchModal');
 
 // Get the button that opens the modal
 const btn = document.getElementById("breakfast-image");
 const lunchBtn = document.getElementById("lunch-image");
 const dinnerBtn = document.getElementById("dinner-image");
+const searchedRecipeModal = document.querySelector('#search-image');
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-const lunchSpan = document.getElementsByClassName("close")[1];
-const dinnerSpan = document.getElementsByClassName("close")[2];
+const searchModalClose = document.getElementsByClassName('close')[0];
+const span = document.getElementsByClassName("close")[1];
+const lunchSpan = document.getElementsByClassName("close")[2];
+const dinnerSpan = document.getElementsByClassName("close")[3];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -171,6 +252,10 @@ lunchBtn.onclick = function () {
 
 dinnerBtn.onclick = function () {
     dinnerModal.style.display = "block";
+}
+
+searchedRecipeModal.onclick = () => {
+    searchModal.style.display = 'block';
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -186,6 +271,11 @@ dinnerSpan.onclick = function () {
     dinnerModal.style.display = "none";
 }
 
+searchModalClose.onclick = () => {
+    searchModal.style.display = 'none';
+}
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target === modal) {
@@ -197,5 +287,9 @@ window.onclick = function (event) {
     if (event.target === dinnerModal) {
         dinnerModal.style.display = "none";
     }
+    if (event.target === searchModal) {
+        searchModal.style.display = 'none';
+    }
 }
 
+document.querySelector('form').addEventListener('submit', searchingData);
